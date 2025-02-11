f4d0edc783669a179895d507c543076d5df99c9d
Simon <Simon-Dirks@users.noreply.github.com>
2025-02-11 16:32:43 +0100

    Disable image classifier

--- src/app/services/camera.service.ts
+++ src/app/services/camera.service.ts
@@ -1,31 +1,34 @@
-import {Injectable} from '@angular/core';
-import {Subject} from 'rxjs';
-import {WebcamImage, WebcamInitError} from 'ngx-webcam';
-import {ImageClassifierService} from './image-classifier.service';
-import {ModalController} from '@ionic/angular';
-import {CameraModalComponent} from '../components/chat/camera-modal/camera-modal.component';
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+import { WebcamImage, WebcamInitError } from "ngx-webcam";
+import { ImageClassifierService } from "./image-classifier.service";
+import { ModalController } from "@ionic/angular";
+import { CameraModalComponent } from "../components/chat/camera-modal/camera-modal.component";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: "root",
 })
 export class CameraService {
-    public trigger: Subject<void> = new Subject<void>();
+  public trigger: Subject<void> = new Subject<void>();
 
-    constructor(public imageClassifier: ImageClassifierService
+  constructor(public imageClassifier: ImageClassifierService) {}
+
+  async handleImage(image: WebcamImage) {
+    // await this.imageClassifier.predictImageClass(image.imageAsDataUrl);
+  }
+
+  public handleInitError(error: WebcamInitError): void {
+    // TODO: Hide app when no camera access has been given + give instructions on how to give it
+    if (
+      error.mediaStreamError &&
+      error.mediaStreamError.name === "NotAllowedError"
     ) {
+      alert(
+        "Please allow camera access for this application to function properly."
+      );
+      return;
     }
-
-    async handleImage(image: WebcamImage) {
-        await this.imageClassifier.predictImageClass(image.imageAsDataUrl);
-    }
-
-    public handleInitError(error: WebcamInitError): void {
-        // TODO: Hide app when no camera access has been given + give instructions on how to give it
-        if (error.mediaStreamError && error.mediaStreamError.name === 'NotAllowedError') {
-            alert('Please allow camera access for this application to function properly.');
-            return;
-        }
-        console.error(error);
-        alert('Something went wrong while initializing the camera..');
-    }
+    console.error(error);
+    alert("Something went wrong while initializing the camera..");
+  }
 }
--- src/app/services/image-classifier.service.ts
+++ src/app/services/image-classifier.service.ts
@@ -1,83 +1,83 @@
-import {Injectable} from '@angular/core';
-import * as tf from '@tensorflow/tfjs';
-import * as mobilenet from '@tensorflow-models/mobilenet';
-import * as knnClassifier from '@tensorflow-models/knn-classifier';
-import {HttpClient} from '@angular/common/http';
+import { Injectable } from "@angular/core";
+// import * as tf from '@tensorflow/tfjs';
+// import * as mobilenet from '@tensorflow-models/mobilenet';
+// import * as knnClassifier from '@tensorflow-models/knn-classifier';
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: "root",
 })
 export class ImageClassifierService {
-    mobileNet;
-    classifier;
-    loadingModel = false;
-    loadingModelStatus = 'Loading model...';
-    lastPrediction = '';
-    readonly CLASSES = ['De Peer', 'The Memory of the Woman-Child', 'Ontmoeting'];
+  mobileNet;
+  classifier;
+  loadingModel = false;
+  loadingModelStatus = "Loading model...";
+  lastPrediction = "";
+  readonly CLASSES = ["De Peer", "The Memory of the Woman-Child", "Ontmoeting"];
 
-    constructor(private http: HttpClient) {
-        // this.loadModel();
-    }
+  constructor(private http: HttpClient) {
+    // this.loadModel();
+  }
 
-    async loadClassifier() {
-        const tensorObj: any = await this.http.get('/assets/data/model.json').toPromise();
-        Object.keys(tensorObj).forEach((key) => {
-            tensorObj[key] = tf.tensor(tensorObj[key], [tensorObj[key].length / 1024, 1024]);
-        });
-        this.classifier.setClassifierDataset(tensorObj);
-    }
+  // async loadClassifier() {
+  //     const tensorObj: any = await this.http.get('/assets/data/model.json').toPromise();
+  //     Object.keys(tensorObj).forEach((key) => {
+  //         tensorObj[key] = tf.tensor(tensorObj[key], [tensorObj[key].length / 1024, 1024]);
+  //     });
+  //     this.classifier.setClassifierDataset(tensorObj);
+  // }
 
-    async loadModel() {
-        console.log('Loading model...');
-        tf.getBackend();
-        this.mobileNet = await mobilenet.load();
-        console.log('Successfully loaded model, now loading classifier...');
-        this.loadingModelStatus = 'Loading classifier...';
-        this.classifier = knnClassifier.create();
-        await this.loadClassifier();
-        console.log('Successfully loaded classifier.');
-        this.loadingModel = false;
-    }
+  // async loadModel() {
+  //     console.log('Loading model...');
+  //     tf.getBackend();
+  //     this.mobileNet = await mobilenet.load();
+  //     console.log('Successfully loaded model, now loading classifier...');
+  //     this.loadingModelStatus = 'Loading classifier...';
+  //     this.classifier = knnClassifier.create();
+  //     await this.loadClassifier();
+  //     console.log('Successfully loaded classifier.');
+  //     this.loadingModel = false;
+  // }
 
-    async predictImageClass(imgUrl: string) {
-        console.log('URL:', imgUrl);
-        const imgTensor: tf.Tensor = await this.getTensorFromImageUrl(imgUrl);
-        return this.predictImageClassFromTensor(imgTensor);
-    }
+  // async predictImageClass(imgUrl: string) {
+  //     console.log('URL:', imgUrl);
+  //     const imgTensor: tf.Tensor = await this.getTensorFromImageUrl(imgUrl);
+  //     return this.predictImageClassFromTensor(imgTensor);
+  // }
 
-    async getTensorFromImageUrl(imageUrl: string): Promise<tf.Tensor> {
-        const img = new Image();
-        img.src = imageUrl;
+  // async getTensorFromImageUrl(imageUrl: string): Promise<tf.Tensor> {
+  //     const img = new Image();
+  //     img.src = imageUrl;
 
-        return new Promise((resolve, reject) => {
-            img.onload = () => {
-                const imgTensor: tf.Tensor = tf.browser.fromPixels(img, 3);
-                resolve(imgTensor);
-            };
-            img.onerror = reject;
-        });
-    }
+  //     return new Promise((resolve, reject) => {
+  //         img.onload = () => {
+  //             const imgTensor: tf.Tensor = tf.browser.fromPixels(img, 3);
+  //             resolve(imgTensor);
+  //         };
+  //         img.onerror = reject;
+  //     });
+  // }
 
-    async predictImageClassFromTensor(imgTensor): Promise<any> {
-        if (!this.classifier || this.classifier.getNumClasses() <= 0) {
-            return Promise.reject('Classifier not properly loaded.');
-        }
+  // async predictImageClassFromTensor(imgTensor): Promise<any> {
+  //     if (!this.classifier || this.classifier.getNumClasses() <= 0) {
+  //         return Promise.reject('Classifier not properly loaded.');
+  //     }
 
-        console.log('Predicting image class...');
+  //     console.log('Predicting image class...');
 
-        const activation = this.mobileNet.infer(imgTensor, 'conv_preds');
-        imgTensor.dispose();
-        const result = await this.classifier.predictClass(activation);
-        const prediction = {
-            prediction: this.CLASSES[result.label],
-            probability: result.confidences[result.label]
-        };
-        this.lastPrediction = prediction.prediction;
+  //     const activation = this.mobileNet.infer(imgTensor, 'conv_preds');
+  //     imgTensor.dispose();
+  //     const result = await this.classifier.predictClass(activation);
+  //     const prediction = {
+  //         prediction: this.CLASSES[result.label],
+  //         probability: result.confidences[result.label]
+  //     };
+  //     this.lastPrediction = prediction.prediction;
 
-        // console.log(`
-        //                   prediction: ${this.CLASSES[result.label]}\n
-        //                   probability: ${result.confidences[result.label]}
-        //                 `);
-        return Promise.resolve(prediction);
-    }
+  //     // console.log(`
+  //     //                   prediction: ${this.CLASSES[result.label]}\n
+  //     //                   probability: ${result.confidences[result.label]}
+  //     //                 `);
+  //     return Promise.resolve(prediction);
+  // }
 }
